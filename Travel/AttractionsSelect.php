18e576a9d55b87cce6ff1666587839793016b17a
kqd5ea <kqd5ea@labunix01.cs.virginia.edu>
2016-04-20 21:19:40 -0400

    Changed form to include food and tours and updated backend

--- Travel/AttractionsSelect.php
+++ Travel/AttractionsSelect.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <table id = "myAttractions">
-    <br/><h1>Attractions<h1>
+    <br/><h1>Things to Do<h1>
     <tr align = center>
-        <th>Name</th><th>Type</th><th>Price</th><th>Address</th>
+        <th>Category</th><th>Name</th><th>Type</th><th>Price</th><th>Location</th>
     </tr>
 <?php 
 	include_once('./library.php'); 
@@ -12,21 +12,107 @@
 		echo("Can't connect to MySQL Server. Error code: " . mysqli_connect_error()); 
 		return null; 
 	} 
-	// Form the SQL query (a SELECT query) 
-	$sql= "SELECT * FROM attractions ORDER BY name"; 
-	$result = mysqli_query($con,$sql); 
-	// Print the data from the table row by row 
-	while($row = mysqli_fetch_array($result)) { 
-		$name = $row['name'];
-		$type = $row['type'];
-		$price = $row['price'];
-		$address = $row['address'];
-		echo "<td>$name</td>"; 
-		echo "<td>$type</td>"; 
-		echo "<td>$$price</td>";
-		echo "<td>$address</td>";  
-		echo "</tr>";
-	} 
+	// Form the SQL query (a SELECT query)
+	$dest = $_POST["destination"];
+	$category = $_POST["category"];
+	if($category == "food"){
+		$sql= "SELECT * FROM restaurants WHERE destination LIKE '%" . $dest . "%' ORDER BY name"; 
+		$result = mysqli_query($con,$sql); 
+		// Print the data from the table row by row 
+		while($row = mysqli_fetch_array($result)) { 
+			$name = $row['name'];
+			$type = $row['cuisine'];
+			$price = $row['price'];
+			$address = $row['address'];
+			echo "<td>Food</td>";
+			echo "<td>$name</td>"; 
+			echo "<td>$type</td>"; 
+			echo "<td>$$price</td>";
+			echo "<td>$address</td>";  
+			echo "</tr>";
+		} 
+	}else if ($category == "tour"){
+		$sql= "SELECT * FROM tours WHERE destination LIKE '%" . $dest . "%' ORDER BY name"; 
+		$result = mysqli_query($con,$sql); 
+		// Print the data from the table row by row 
+		while($row = mysqli_fetch_array($result)) { 
+			$name = $row['name'];
+			$type = $row['type'];
+			$price = $row['price'];
+			$address = $row['destination'];
+			echo "<td>Tour</td>";
+			echo "<td>$name</td>"; 
+			echo "<td>$type</td>"; 
+			echo "<td>$$price</td>";
+			echo "<td>$address</td>";  
+			echo "</tr>";
+		} 
+	}else{
+		if($category == NULL){
+			$sql= "SELECT * FROM attractions WHERE destination LIKE '%" . $dest . "%' ORDER BY name"; 
+			$result = mysqli_query($con,$sql); 
+			// Print the data from the table row by row 
+			while($row = mysqli_fetch_array($result)) { 
+				$name = $row['name'];
+				$type = $row['type'];
+				$price = $row['price'];
+				$address = $row['address'];
+				echo "<td>Attraction</td>";
+				echo "<td>$name</td>"; 
+				echo "<td>$type</td>"; 
+				echo "<td>$$price</td>";
+				echo "<td>$address</td>";  
+				echo "</tr>";
+			} 
+			$sql= "SELECT * FROM tours WHERE destination LIKE '%" . $dest . "%' ORDER BY name"; 
+			$result = mysqli_query($con,$sql); 
+			// Print the data from the table row by row 
+			while($row = mysqli_fetch_array($result)) { 
+				$name = $row['name'];
+				$type = $row['type'];
+				$price = $row['price'];
+				$address = $row['destination'];
+				echo "<td>Tour</td>";
+				echo "<td>$name</td>"; 
+				echo "<td>$type</td>"; 
+				echo "<td>$$price</td>";
+				echo "<td>$address</td>";  
+				echo "</tr>";
+			} 
+			$sql= "SELECT * FROM restaurants WHERE destination LIKE '%" . $dest . "%' ORDER BY name"; 
+			$result = mysqli_query($con,$sql); 
+			// Print the data from the table row by row 
+			while($row = mysqli_fetch_array($result)) { 
+				$name = $row['name'];
+				$type = $row['cuisine'];
+				$price = $row['price'];
+				$address = $row['address'];
+				echo "<td>Food</td>";
+				echo "<td>$name</td>"; 
+				echo "<td>$type</td>"; 
+				echo "<td>$$price</td>";
+				echo "<td>$address</td>";  
+				echo "</tr>";
+			} 
+		}else{
+			$sql= "SELECT * FROM attractions WHERE destination LIKE '%" . $dest . "%' AND type='". $category ."' ORDER BY name"; 
+			$result = mysqli_query($con,$sql); 
+			// Print the data from the table row by row 
+			while($row = mysqli_fetch_array($result)) { 
+				$name = $row['name'];
+				$type = $row['type'];
+				$price = $row['price'];
+				$address = $row['address'];
+				echo "<td>$category</td>"; 
+				echo "<td>$name</td>"; 
+				echo "<td>$type</td>"; 
+				echo "<td>$$price</td>";
+				echo "<td>$address</td>";  
+				echo "</tr>";
+			} 
+		}
+	}
+
 	mysqli_close($db_connection); 
 ?> 
 </table>
