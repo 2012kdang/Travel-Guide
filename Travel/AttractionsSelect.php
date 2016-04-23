4b7c13f2a81fc9838e6060eaed54d4782719244c
kqd5ea <kqd5ea@labunix01.cs.virginia.edu>
2016-04-23 13:43:47 -0400

    added AjAX

--- Travel/AttractionsSelect.php
+++ Travel/AttractionsSelect.php
@@ -1,119 +1,29 @@
-<!DOCTYPE html>
-<table id = "myAttractions">
-    <br/><h1>Things to Do<h1>
-    <tr align = center>
-        <th>Category</th><th>Name</th><th>Type</th><th>Price</th><th>Location</th>
-    </tr>
-<?php 
-	include_once('./library.php'); 
-	$con = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE); 
-	// Check connection 
-	if (mysqli_connect_errno()) { 
-		echo("Can't connect to MySQL Server. Error code: " . mysqli_connect_error()); 
-		return null; 
-	} 
-	// Form the SQL query (a SELECT query)
-	$dest = $_POST["destination"];
-	$category = $_POST["category"];
-	if($category == "food"){
-		$sql= "SELECT * FROM restaurants WHERE destination LIKE '%" . $dest . "%' ORDER BY name"; 
-		$result = mysqli_query($con,$sql); 
-		// Print the data from the table row by row 
-		while($row = mysqli_fetch_array($result)) { 
-			$name = $row['name'];
-			$type = $row['cuisine'];
-			$price = $row['price'];
-			$address = $row['address'];
-			echo "<td>Food</td>";
-			echo "<td>$name</td>"; 
-			echo "<td>$type</td>"; 
-			echo "<td>$$price</td>";
-			echo "<td>$address</td>";  
-			echo "</tr>";
-		} 
-	}else if ($category == "tour"){
-		$sql= "SELECT * FROM tours WHERE destination LIKE '%" . $dest . "%' ORDER BY name"; 
-		$result = mysqli_query($con,$sql); 
-		// Print the data from the table row by row 
-		while($row = mysqli_fetch_array($result)) { 
-			$name = $row['name'];
-			$type = $row['type'];
-			$price = $row['price'];
-			$address = $row['destination'];
-			echo "<td>Tour</td>";
-			echo "<td>$name</td>"; 
-			echo "<td>$type</td>"; 
-			echo "<td>$$price</td>";
-			echo "<td>$address</td>";  
-			echo "</tr>";
-		} 
-	}else{
-		if($category == NULL){
-			$sql= "SELECT * FROM attractions WHERE destination LIKE '%" . $dest . "%' ORDER BY name"; 
-			$result = mysqli_query($con,$sql); 
-			// Print the data from the table row by row 
-			while($row = mysqli_fetch_array($result)) { 
-				$name = $row['name'];
-				$type = $row['type'];
-				$price = $row['price'];
-				$address = $row['address'];
-				echo "<td>Attraction</td>";
-				echo "<td>$name</td>"; 
-				echo "<td>$type</td>"; 
-				echo "<td>$$price</td>";
-				echo "<td>$address</td>";  
-				echo "</tr>";
-			} 
-			$sql= "SELECT * FROM tours WHERE destination LIKE '%" . $dest . "%' ORDER BY name"; 
-			$result = mysqli_query($con,$sql); 
-			// Print the data from the table row by row 
-			while($row = mysqli_fetch_array($result)) { 
-				$name = $row['name'];
-				$type = $row['type'];
-				$price = $row['price'];
-				$address = $row['destination'];
-				echo "<td>Tour</td>";
-				echo "<td>$name</td>"; 
-				echo "<td>$type</td>"; 
-				echo "<td>$$price</td>";
-				echo "<td>$address</td>";  
-				echo "</tr>";
-			} 
-			$sql= "SELECT * FROM restaurants WHERE destination LIKE '%" . $dest . "%' ORDER BY name"; 
-			$result = mysqli_query($con,$sql); 
-			// Print the data from the table row by row 
-			while($row = mysqli_fetch_array($result)) { 
-				$name = $row['name'];
-				$type = $row['cuisine'];
-				$price = $row['price'];
-				$address = $row['address'];
-				echo "<td>Food</td>";
-				echo "<td>$name</td>"; 
-				echo "<td>$type</td>"; 
-				echo "<td>$$price</td>";
-				echo "<td>$address</td>";  
-				echo "</tr>";
-			} 
-		}else{
-			$sql= "SELECT * FROM attractions WHERE destination LIKE '%" . $dest . "%' AND type='". $category ."' ORDER BY name"; 
-			$result = mysqli_query($con,$sql); 
-			// Print the data from the table row by row 
-			while($row = mysqli_fetch_array($result)) { 
-				$name = $row['name'];
-				$type = $row['type'];
-				$price = $row['price'];
-				$address = $row['address'];
-				echo "<td>$category</td>"; 
-				echo "<td>$name</td>"; 
-				echo "<td>$type</td>"; 
-				echo "<td>$$price</td>";
-				echo "<td>$address</td>";  
-				echo "</tr>";
-			} 
-		}
-	}
 
-	mysqli_close($db_connection); 
-?> 
-</table>
-</html>
+<?php
+	require "dbutil.php";
+	$db = DbUtil::loginConnection();
+	
+	$stmt = $db->stmt_init();
+
+	$stringDest = $_GET['searchDestination'];
+	$stringType = $_GET['searchCategory'];
+
+	if($stmt->prepare("select * from attractions where destination like ? AND type like ?") or die(mysqli_error($db))) {
+		$stringDest = '%' . $_GET['searchDestination'] . '%';
+		$stringType = '%' . $_GET['searchCategory'] . '%';
+		$stmt->bind_param('ss', $stringDest, $stringType);
+		$stmt->execute();
+		$stmt->bind_result($type, $price, $name, $address, $destination);
+		echo "<table border=1><th>Category</th><th>Name</th><th>Type</th><th>Price</th><th>Location</th>\n";
+		while($stmt->fetch()) {
+			echo "<tr><td>Attraction</td><td>$name</td><td>$type</td><td>$price</td><td>$address</td></tr>";
+		}
+		echo "</table>";
+	
+		$stmt->close();
+	}
+	
+	$db->close();
+
+
+?>
